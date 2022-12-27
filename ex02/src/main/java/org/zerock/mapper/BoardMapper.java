1383e30ecd42412062d2a91b68795cf17517619e
ayj8487 <ayj8487@naver.com>
2022-12-27 23:00:14 +0900

    board InsertTest

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -8,8 +8,14 @@ import org.zerock.domain.BoardVO;
 public interface BoardMapper {
 	// 간단한 SQL쿼리라면 @어노테이션으로 처리할 수 있지만
 	// 복잡한 쿼리나 상황에 따라다른 SQL 쿼리를 사용하려면 MyBatis를 활용해 XML로 대체 한다 
-//	@Select("select * from tbl_board where bno > 0")
+
+	//	@Select("select * from tbl_board where bno > 0")
 
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	// insert만 처리되고 생성된PK (bno) 값을 알 필요가 없는경우
+	public void insertSelectKey(BoardVO board);
+	// insert문이 실행되고 생성된PK 값을 알야야 하는 경우
 	
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -18,8 +19,33 @@ public class BoardMapperTests {
 	@Setter(onMethod_ =@Autowired )
 	private BoardMapper mapper;
 	
+	// 게시물 리스트 테스트
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+	// 게시물 등록 테스트 (insert)
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로작성 제목 테스트");
+		board.setContent("새로작성 내용 테스트");
+		board.setWriter("new11");
+		
+		mapper.insert(board);
+		
+		log.info(board); // Lombok이 만들어주는 toString()을 이용해서 bno 멤버 변수의 값을 알아보기위해
+	}
+	
+	// 게시물 등록 테스트(insertSelectKey)
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로작성 제목 테스트 selectKey");
+		board.setContent("새로작성 내용 테스트 selectKey");
+		board.setWriter("new11");
+		
+		mapper.insertSelectKey(board);
+		log.info(board);
 	}
 }
