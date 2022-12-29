6a25d359ba7b2aca17f573695de322271287fe1d
ayj8487 <ayj8487@naver.com>
2022-12-30 04:20:29 +0900

    CRUD 등록,수정,조회,삭제 Tomcat없이 Controller 테스트

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -1,10 +1,13 @@
 package org.zerock.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
 import lombok.AllArgsConstructor;
@@ -21,7 +24,7 @@ public class BoardController {
 	private BoardService service; //BoardService 타입의 객체와 같이 연동
 
 	
-	//게시물 전체 목록
+	// 게시물 전체 목록
 	// list는 나중에 게시물목록을 전달해야 하므로 Model을 파라미터로 지정 하고 이를통해 BoardServiceImpl 객체의 getList() 결과를 담아 전달(addAttribute) 함
 	@GetMapping("/list")
 	public void list (Model model) { 
@@ -29,4 +32,47 @@ public class BoardController {
 		
 		model.addAttribute("list", service.getList());
 	}
+	// 게시물 등록
+	//String  타입으로 지정하고 RedirectAttributes 를 파라미터로 지정
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("regist : " + board);
+		
+		service.register(board);
+		
+		rttr.addFlashAttribute("result",board.getBno());
+		
+		return "redirect:/board/list"; //redirect: 접두어 사용시 내부적으로 response.secdRedirect() 처리
+	}
+	// 게시물 조회
+	//bno 값을 더 명시적으로 처리하기위해 @RequestParam사용, 화면쪽으로 해당 번호의 게시물을 전달해야하므로 Model파라미터 지정
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) { 
+		log.info("/get");
+		model.addAttribute("board",service.get(bno));
+	}
+	// 게시물 수정
+	// 변경된 내용을 수집하여 BoardVO 파라미터로 처리하고 BoardService를 호출
+	// 수정 작업을 시작하는 화면의 경우 GET방식이지만 실제 작업은 POST이기에 POSTMapping사용
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify :" + board);
+		
+		//수정 여부를 boolean으로 처리하므로 이를 성공한경우에만 RedirectAttributes에 추가
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+	}
+	// 게시물 삭제
+	// 삭제는 반드시 post 방식으로만 처리,
+	// remove()메서드로 삭제 시킨 후 이동할 페이지가 필요하기에 RedirectAttributes 파라미터 사용
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove :" + bno);
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/board/list";
+}
 }
--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -39,14 +39,60 @@ public class BoardControllerTests {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
 		// mockMvc = 가짜 MVC, 가짜로 URL과 파라미터 등을 브라우저에서 사용하는 것처럼 만듦
 	}
+	
+	// 게시물 리스트 테스트
+//	@Test
+//	// MockMvcRequestBuilders = GET방식의 호출 
+//	public void testList() throws Exception {
+//		log.info(
+//				mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+//				.andReturn()
+//				.getModelAndView()
+//				.getModelMap());
+//	}
+	
+	// 게시물 등록 테스트
+//	@Test
+//	//MockMvcRequestBuilders = POST방식의 호출
+//	public void testRegister() throws Exception {
+//		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+//				.param("title", "테스트 새글 제목") //.param()을 통해 전달해야 하는 파라미터를 지정 <input>태그와 유사
+//				.param("content", "테스트 새글 내용")
+//				.param("writer", "aaa123")).andReturn().getModelAndView().getViewName();
+//		log.info(resultPage);
+//	}
+	
+	// 게시물 조회 테스트
+//	// 특정 게시물을 조회할 땐 반드시 'bno'파라미터가 필요하므로 param() 을 통해 추가
+//	@Test
+//	public void testGet() throws Exception{
+//		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/get")
+//				.param("bno", "2"))
+//				.andReturn()
+//				.getModelAndView().getModelMap());
+//	}
 
-	@Test
-	// MockMvcRequestBuilders = GET방식의 호출 
-	public void testList() throws Exception {
-		log.info(
-				mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
-				.andReturn()
-				.getModelAndView()
-				.getModelMap());
-	}
+	// 게시물 수정 테스트
+//	@Test
+//	public void testModify() throws Exception{
+//		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify")
+//				.param("bno", "1")
+//				.param("title","수정된 테스트 새글 제목")
+//				.param("content", "수정된 새글 내용")
+//				.param("writer", "bbb123"))
+//				.andReturn().getModelAndView().getViewName();
+//		
+//		log.info(resultPage);
+//	}
+	
+	// 게시물 삭제 테스트
+	// MockMvc파라미터로 전달할때는 문자열로만 처리, 삭제전 데이터베이스 게시물 확인
+//	@Test
+//	public void testRemove() throws Exception{
+//		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+//				.param("bno", "21")).andReturn().getModelAndView().getViewName();
+//		
+//		log.info(resultPage);
+//	}
+	
 }
