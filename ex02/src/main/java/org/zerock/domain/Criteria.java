bee31d4ee7a85979293116dc65d404a41bbfc6a1
ayj8487 <ayj8487@naver.com>
2023-01-10 18:42:30 +0900

    검색 기능 구현 전 DTO 추가 Criteria 클래스 수정

--- ex02/src/main/java/org/zerock/domain/Criteria.java
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -17,9 +17,15 @@ public class Criteria { // 검색의 "기준"
  	페이징 처리의 BoardMapper는 인터페이스와 어노테이션을 이용하기 때문에 SQL 구문 처리가 복잡하다.
 	때문에 XML 로 처리하여 알아보기 쉽고 관리하기 쉽게 처리
 */	
+	// 페이징
 	private int pageNum; // 페이지 번호
 	private int amount; // 한페이지당 몇 개의 데이터
-
+	
+	// 검색
+	private String type;
+	private String keyword;
+	
+	
 	public Criteria() { // 생성자를 통해서 기본값을 1페이지, 10개로 지정처리 
 		this(1,10);
 	}
@@ -28,7 +34,11 @@ public class Criteria { // 검색의 "기준"
 		this.pageNum = pagNum;
 		this.amount = amount;
 	}
-	
+
+	// 검색시 MyBatis 동적태그(if, choose, trim, forEach 등) 를 배열로 처리하여 활용하기 위한 메서드 추가
+	public String[] getTypeArr() {
+		return type == null? new String[] {}: type.split("");
+	}
 	
 
 }
